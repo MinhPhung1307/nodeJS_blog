671189bcb15cfb5164679ca4255c04679b1ab78c
Nguyen Minh Phung <nguyenminhphung1307@gmail.com>
2025-05-08 16:46:52 +0700

    Autoincrement _id field

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 // const slug = require('mongoose-slug-generator');
 const slug = require('mongoose-slug-updater');
@@ -9,14 +10,18 @@ const mongooseDelete = require('mongoose-delete');
 
 
 const Course = new Schema({
+  _id: { type: Number },
   name: { type: String, required: true },
   description: { type: String },
   image: { type: String },
   videoId: { type: String, required: true },
   slug: { type: String, slug: 'name', unique: true},
-}, { 
+}, {
+  _id: false,
   timestamps: true
 });
+
+Course.plugin(AutoIncrement);
 
 Course.plugin(mongooseDelete, {
   deletedAt : true,
